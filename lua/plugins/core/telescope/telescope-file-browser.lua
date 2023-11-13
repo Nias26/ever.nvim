eff86696f0b9b8d8eaaeae8c5ed10181c6ed8f3e
Nias26 <genangeliludovico26@gmail.com>
2023-11-13 17:33:42 +0100

    Fix telescope file browser not showing in telescope extensions

--- lua/plugins/core/telescope/telescope-file-browser.lua
+++ lua/plugins/core/telescope/telescope-file-browser.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-telescope/telescope-file-browser.nvim",
-	lazy = true,
+	event = "VeryLazy",
    dependencies = { "nvim-telescope/telescope.nvim" },
    config = function()
    	require("telescope").load_extension("file_browser")
