dd423b4f3e6caff88e3f0b10cdb085952662d5f6
Nias26 <genangeliludovico26@gmail.com>
2023-11-16 15:45:07 +0100

    Make functions from global to local

--- lua/macros/functions.lua
+++ lua/macros/functions.lua
@@ -1,18 +1,16 @@
 -- Vim keymaps
-function map(mode, lhs, rhs, opts)
+local function map(mode, lhs, rhs, opts)
     local options = {noremap = true}
     if opts then options = vim.tbl_extend('force', options, opts) end
     vim.keymap.set(mode, lhs, rhs, options)
 end
 
 -- vim.cmd
-function fcmd(command, opts)
+local function fcmd(command, opts)
 	vim.cmd({ cmd = command, args = {opts}})
 end
 
--- Open LSP Diagnostic window over error, warn, info, ...
--- Function to check if a floating dialog exists and if not then check for diagnostics under the cursor
-function OpenDiagnosticIfNoFloat()
+local function OpenDiagnosticIfNoFloat()
 	for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
    	if vim.api.nvim_win_get_config(winid).zindex then
       	return
@@ -31,10 +29,9 @@ function OpenDiagnosticIfNoFloat()
       },
    })
 end
--- Show diagnostics under the cursor when holding position
-vim.api.nvim_create_augroup("lsp_diagnostics_hold", { clear = true })
-vim.api.nvim_create_autocmd({ "CursorHold" }, {
-	pattern = "*",
-   command = "lua OpenDiagnosticIfNoFloat()",
-   group = "lsp_diagnostics_hold",
-})
+
+return {
+	map = map,
+	fcmd = fcmd,
+	OpenDiagnosticIfNoFloat = OpenDiagnosticIfNoFloat
+}
