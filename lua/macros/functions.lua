10fe882bab513e1f5c6271358281357674f81a71
Nias26 <genangeliludovico26@gmail.com>
2023-11-18 10:54:22 +0100

    Set pcall and removed unused func

--- lua/macros/functions.lua
+++ lua/macros/functions.lua
@@ -1,21 +1,16 @@
--- Vim keymaps
-local function map(mode, lhs, rhs, opts)
-    local options = {noremap = true}
-    if opts then options = vim.tbl_extend('force', options, opts) end
-    vim.keymap.set(mode, lhs, rhs, options)
-end
-
 -- vim.cmd
 local function fcmd(command, opts)
 	vim.cmd({ cmd = command, args = {opts}})
 end
 
+-- Open LSP Diagnostic window over error, warn, info, ...
+-- Function to check if a floating dialog exists and if not then check for diagnostics under the cursor
 local function OpenDiagnosticIfNoFloat()
-	for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
-   	if vim.api.nvim_win_get_config(winid).zindex then
-      	return
-      end
+for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+	if vim.api.nvim_win_get_config(winid).zindex then
+   	return
    end
+end
    -- THIS IS FOR BUILTIN LSP
    vim.diagnostic.open_float(0, {
    	scope = "cursor",
@@ -31,7 +26,6 @@ local function OpenDiagnosticIfNoFloat()
 end
 
 return {
-	map = map,
 	fcmd = fcmd,
 	OpenDiagnosticIfNoFloat = OpenDiagnosticIfNoFloat
 }
--- lua/macros/init.lua
+++ lua/macros/init.lua
@@ -1,23 +1,10 @@
--- set colorscheme
--- vim.cmd("colorscheme oxocarbon")
-require("current-theme")
+-- Set colorscheme
+local status_ok, _ = pcall(require("current-theme"))
+if not status_ok then
+	return
+end
+
 f = require("macros.functions")
-
--- Set Number line
-vim.opt.relativenumber = true
-vim.opt.number = true
-
--- Commands preview
-vim.opt.inccommand = "split"
-
--- Ignore uppercase and lowercase in commands
-vim.opt.ignorecase = true
-
--- Set global statusbar
-vim.opt.laststatus = 3
-
--- Set scrolloff
-vim.opt.scrolloff = 999
 
 -- Set Virtual text
 vim.diagnostic.config({
@@ -27,29 +14,38 @@ vim.diagnostic.config({
 	update_in_insert = false,
 	seveirty_sort = false,
 	float = {
-   	source = "always",
-   },
+		source = "always",
+	},
+})
+
+-- Open LSP Diagnostic window over error, warn, info, ...
+-- Show diagnostics under the cursor when holding position
+vim.api.nvim_create_augroup("lsp_diagnostics_hold", { clear = true })
+vim.api.nvim_create_autocmd({ "CursorHold" }, {
+	pattern = "*",
+	command = "lua f.OpenDiagnosticIfNoFloat()",
+	group = "lsp_diagnostics_hold",
 })
 
 -- Quit nvim if nvim-tree is the last open buffer
 vim.api.nvim_create_autocmd("QuitPre", {
 	callback = function()
-   	local tree_wins = {}
-	   local floating_wins = {}
-	   local wins = vim.api.nvim_list_wins()
-	   for _, w in ipairs(wins) do
+		local tree_wins = {}
+		local floating_wins = {}
+		local wins = vim.api.nvim_list_wins()
+		for _, w in ipairs(wins) do
 			local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
-		   if bufname:match("NvimTree_") ~= nil then
-		   	table.insert(tree_wins, w)
+			if bufname:match("NvimTree_") ~= nil then
+				table.insert(tree_wins, w)
 			end
-		   if vim.api.nvim_win_get_config(w).relative ~= '' then
-		   	table.insert(floating_wins, w)
+			if vim.api.nvim_win_get_config(w).relative ~= '' then
+				table.insert(floating_wins, w)
 			end
 		end
 		if 1 == #wins - #floating_wins - #tree_wins then
 			-- Should quit, so we close all invalid windows.
-		   for _, w in ipairs(tree_wins) do
-		   	vim.api.nvim_win_close(w, true)
+			for _, w in ipairs(tree_wins) do
+				vim.api.nvim_win_close(w, true)
 			end
 		end
 	end
@@ -60,13 +56,3 @@ vim.api.nvim_create_user_command('Q', 'q', {})
 vim.api.nvim_create_user_command('Wq', 'wq', {})
 vim.api.nvim_create_user_command('Qa', 'qa', {})
 vim.api.nvim_create_user_command('Wqa', 'wqa', {})
-
--- Open LSP Diagnostic window over error, warn, info, ...
--- Function to check if a floating dialog exists and if not then check for diagnostics under the cursor
--- Show diagnostics under the cursor when holding position
-vim.api.nvim_create_augroup("lsp_diagnostics_hold", { clear = true })
-vim.api.nvim_create_autocmd({ "CursorHold" }, {
-	pattern = "*",
-   command = "lua f.OpenDiagnosticIfNoFloat()",
-   group = "lsp_diagnostics_hold",
-})
