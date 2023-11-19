77c3d6fc273ad14b8b9ca375f5c75fcd2b4efa55
Nias26 <genangeliludovico26@gmail.com>
2023-11-19 22:08:54 +0100

    NEW: Added [lspsaga.nvim]
    This plugin makes LSP really easy and good as it provides a beautiful UI with vim.ui
    and many modules

---
+++ lua/plugins/lsp/lspsaga.lua
@@ -0,0 +1,33 @@
+return{
+	"nvimdev/lspsaga.nvim",
+	event = "LspAttach",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons"
+	},
+	config = function()
+		local status_ok, lspsaga = pcall(require, "lspsaga")
+		if not status_ok then
+			return
+		end
+
+		lspsaga.setup({
+			ui = {
+				code_action = "",
+				border = 'solid',
+				expand = '',
+				collapse = '',
+			},
+			symbol_in_winbar = {
+				enable = false
+			},
+			lightbulb = {
+				enable = true,
+				sign = false
+			},
+			implement = {
+				enable = false
+			}
+		})
+	end
+}
