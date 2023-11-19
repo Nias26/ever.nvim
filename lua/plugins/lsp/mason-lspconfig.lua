5de19a527cef6bb54300d5d530cbbf28ace4431a
Nias26 <genangeliludovico26@gmail.com>
2023-11-19 22:07:25 +0100

    FIX: 'vim' as a undeclared variable

--- lua/plugins/lsp/mason-lspconfig.lua
+++ lua/plugins/lsp/mason-lspconfig.lua
@@ -1,30 +1,35 @@
 return {
 	"williamboman/mason-lspconfig.nvim",
 	event = "VeryLazy",
-   dependencies = {
-   	"williamboman/mason.nvim",
-      "neovim/nvim-lspconfig"
-   },
-   config = function()
-   	require("mason").setup()
-      require("mason-lspconfig").setup{
+	dependencies = {
+		"williamboman/mason.nvim",
+		"neovim/nvim-lspconfig"
+	},
+	config = function()
+		require("mason").setup()
+		require("mason-lspconfig").setup{
 			ensure_installed = { 'lua_ls', 'clangd', 'bashls' },
 			automatic_installation = true
 		}
 
-      require("mason-lspconfig").setup_handlers{
-	   	-- The first entry (without a key) will be the default handler
-         -- and will be called for each installed server that doesn't have
-         -- a dedicated handler.
-         function(server_name) -- Default handler (Optional)
-         	require("lspconfig")[server_name].setup{}
+		require("mason-lspconfig").setup_handlers{
+			-- The first entry (without a key) will be the default handler
+			-- and will be called for each installed server that doesn't have
+			-- a dedicated handler.
+			function(server_name) -- Default handler (Optional)
+				require("lspconfig")[server_name].setup{}
 				local capabilities = require("cmp_nvim_lsp").default_capabilities()
-         end,
-      -- Next, you can provide a dedicated handler for specific servers.
-      -- For example, a handler override for the `rust_analyzer`:
-      -- ["rust_analyzer"] = function ()
-      	-- require("rust-tools").setup {}
-      -- end
-	   }
-   end
+			end,
+
+			require('lspconfig').lua_ls.setup{
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" }
+						}
+					}
+				}
+			}
+		}
+	end
 }
