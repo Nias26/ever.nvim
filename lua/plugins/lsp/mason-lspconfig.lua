ac63654e66c542da93d575f86f3b15cd52900571
Nias26 <genangeliludovico26@gmail.com>
2023-11-23 19:40:05 +0100

    NEW: Added capabilities and on_attach functions on lsp servers

--- lua/plugins/lsp/mason-lspconfig.lua
+++ lua/plugins/lsp/mason-lspconfig.lua
@@ -7,27 +7,31 @@ return {
 	},
 	config = function()
 		require("mason").setup()
-		require("mason-lspconfig").setup{
+		require("mason-lspconfig").setup {
 			ensure_installed = { 'lua_ls', 'clangd', 'bashls' },
-			automatic_installation = true
 		}
 
-		require("mason-lspconfig").setup_handlers{
+		require("mason-lspconfig").setup_handlers {
 			-- The first entry (without a key) will be the default handler
 			-- and will be called for each installed server that doesn't have
 			-- a dedicated handler.
 			function(server_name) -- Default handler (Optional)
-				require("lspconfig")[server_name].setup{}
+				require("lspconfig")[server_name].setup {
+					capabilities = capabilities,
+					on_attach = on_attach
+				}
 				local capabilities = require("cmp_nvim_lsp").default_capabilities()
-				on_attach = on_attach
 			end,
 
-			require('lspconfig').lua_ls.setup{
+			require('lspconfig').lua_ls.setup {
 				settings = {
 					Lua = {
 						diagnostics = {
 							globals = { "vim" }
-						}
+						},
+						workspace = {
+							checkThirdParty = false,
+						},
 					}
 				}
 			}
