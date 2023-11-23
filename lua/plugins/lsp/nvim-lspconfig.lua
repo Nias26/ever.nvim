bfeb123977617c9d8200a3768fba41edddc5214d
Nias26 <genangeliludovico26@gmail.com>
2023-11-23 19:40:24 +0100

    UPDATE: Removed not working noice lsp status feature

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -1,22 +1,11 @@
 return {
 	"neovim/nvim-lspconfig",
 	event = "VeryLazy",
-   config = function()
+	config = function()
 		local signs = { Error = " ", Warn = " ", Hint = "󰌵 ", Info = "󱈸 " }
-      for type, icon in pairs(signs) do
-      	local hl = "DiagnosticSign" .. type
-         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-      end
-
-		-- Fix for some lua_ls errors
-		require("lspconfig").lua_ls.setup({
-			settings = {
-				Lua = {
-					workspace = {
-						checkThirdParty = false,
-					},
-				}
-			}
-		})
-   end
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+		end
+	end
 }
