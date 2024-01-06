15a21c98d3220a372c247fabdb1ab1b88a55ee01
Nias26 <genangeliludovico26@gmail.com>
2024-01-06 13:14:37 +0100

    New: Added lualine.nvim
    Remove: Remove feline.nvim (Archived)

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -84,15 +84,15 @@ return {
 			},
 			signature = { enabled = false }
 		})
-		-- require("lualine").setup({
-		-- sections = {
-		-- lualine_x = {{
-		-- require("noice").api.statusline.mode.get,
-		-- cond = require("noice").api.statusline.mode.has,
-		-- color = { fg = "#ff9e64" },
-		-- }},
-		-- },
-		-- })
+		require("lualine").setup({
+			sections = {
+				lualine_x = {{
+					require("noice").api.statusline.mode.get,
+					cond = require("noice").api.statusline.mode.has,
+					color = { fg = "#ff9e64" },
+				}},
+			},
+		})
 		require("telescope").load_extension("noice")
 	end
 }
