1baf812622703a7aa189e7a9f8ed1af9e773db63
Nias26 <genangeliludovico26@gmail.com>
2024-01-19 19:22:30 +0100

    New: project.nvim pattern for projects

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -5,7 +5,7 @@ return {
 		require("project_nvim").setup {
 			manual_mode = false,
 			detection_methods = { "pattern" },
-			patterns = { ".git", "Makefile", },
+			patterns = { ".git", "Makefile", ".project" },
 			show_hidden = false,
 			silent_chdir = true,
 			scope_chdir = 'win',
