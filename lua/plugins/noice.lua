060e42439b042989dd297b5a12b753e10e261da8
Nias26 <genangeliludovico26@gmail.com>
2024-01-29 18:00:03 +0100

    remove(noice): unecessary features

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -40,21 +40,6 @@ return {
 					input = {},
 				},
 			},
-			messages = {
-				-- NOTE: If you enable messages, then the cmdline is enabled automatically.
-				-- This is a current Neovim limitation.
-				enabled = true, -- enables the Noice messages UI
-				view = "notify", -- default view for messages
-				view_error = "notify", -- view for errors
-				view_warn = "notify", -- view for warnings
-				view_history = "messages", -- view for :messages
-				view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
-			},
-			popupmenu = {
-				enabled = true,
-				backend = "nui",
-				kind_icons = {},
-			},
 			notify = {
 				enabled = true,
 				view = "mini",
@@ -71,56 +56,12 @@ return {
 					["vim.lsp.util.convert_input_to_markdown_lines"] = false,
 					["vim.lsp.util.stylize_markdown"] = false,
 				},
-				hover = {
-					enabled = true,
-					silent = false, -- set to true to not show a message if hover is not available
-					view = nil, -- when nil, use defaults from documentation
-					opts = {}, -- merged with defaults from documentation
-				},
-				signature = {
-					enabled = false,
-					auto_open = {
-						enabled = true,
-						trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
-						luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
-						throttle = 50, -- Debounce lsp signature help request by 50ms
-					},
-					view = nil, -- when nil, use defaults from documentation
-					---@type NoiceViewOptions
-					opts = {}, -- merged with defaults from documentation
-				},
 				message = {
 					-- Messages shown by lsp servers
 					enabled = true,
 					view = "mini",
 					opts = {},
 				},
-				documentation = {
-					view = "hover",
-					---@type NoiceViewOptions
-					opts = {},
-				},
-			},
-			markdown = {
-				hover = {
-					["|(%S-)|"] = vim.cmd.help, -- vim help links
-					["%[.-%]%((%S-)%)"] = require("noice.util").open, -- markdown links
-				},
-				highlights = {
-					["|%S-|"] = "@text.reference",
-					["@%S+"] = "@parameter",
-					["^%s*(Parameters:)"] = "@text.title",
-					["^%s*(Return:)"] = "@text.title",
-					["^%s*(See also:)"] = "@text.title",
-					["{%S-}"] = "@parameter",
-				},
-			},
-			health = {
-				checker = true, -- Disable if you don't want health checks to run
-			},
-			smart_move = {
-				-- noice tries to move out of the way of existing floating windows.
-				enabled = false, -- you can disable this behaviour here
 			},
 			presets = {
 				bottom_search = false, -- use a classic bottom cmdline for search
@@ -129,7 +70,6 @@ return {
 				inc_rename = true, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = false, -- add a border to hover docs and signature help
 			},
-			throttle = 1000 / 30, -- how frequently does Noice need to check for ui updates? This has no effect when in blocking mode.
 			routes = {
 				{
 					view = "mini",
