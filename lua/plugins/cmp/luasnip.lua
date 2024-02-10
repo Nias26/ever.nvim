ec12edae0998853f9ddc99879de17d814f4bfbf2
Nias26 <genangeliludovico26@gmail.com>
2024-02-10 12:59:17 +0100

    remove(LuaSnip): uneccessary opt

--- lua/plugins/cmp/luasnip.lua
+++ lua/plugins/cmp/luasnip.lua
@@ -2,8 +2,7 @@ return {
 	"L3MON4D3/LuaSnip",
 	event = "BufRead",
 	dependencies = { "rafamadriz/friendly-snippets" },
-	build = "make install_jsregexp",
 	config = function()
 		require("luasnip.loaders.from_vscode").lazy_load()
-	end
+	end,
 }
