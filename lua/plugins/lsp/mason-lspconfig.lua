5a7d1c8bcfd2e10bc6934378cab833345946b78b
Nias26 <genangeliludovico26@gmail.com>
2024-04-06 15:04:23 +0200

    plugins(lsp): moved server configs from mason-lspconfig to nvim-lspconfig
    and removed unecessary servers

--- lua/plugins/lsp/mason-lspconfig.lua
+++ lua/plugins/lsp/mason-lspconfig.lua
@@ -1,5 +1,4 @@
 return {
-  -- TODO:Review mason-lspconfig usage 
 	"williamboman/mason-lspconfig.nvim",
 	event = "BufReadPre",
 	dependencies = {
@@ -11,54 +10,8 @@ return {
 		require("mason-lspconfig").setup({
 			ensure_installed = {
 				lsp = { "lua_ls", "clangd" },
-				linter = { "luacheck", "cpplint" },
 				formatter = { "stylua", "clang-format" },
 			},
 		})
-		capabilities = require("cmp_nvim_lsp").default_capabilities()
-		on_attach = function(client, bufnr)
-			require("nvim-navic").attach(client, bufnr)
-		end
-
-		local lspconfig = require("lspconfig")
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = {
-				Lua = {
-					completion = {
-						callSnippet = "Replace",
-					},
-					diagnostics = {
-						-- Get the language server to recognize the `vim` global
-						globals = { "vim" },
-					},
-				},
-			},
-		})
-
-		lspconfig.clangd.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			root_dir = lspconfig.util.root_pattern("*.cpp"),
-		})
-
-		lspconfig.eslint.setup({
-			capabilities = capabilities,
-			-- on_attach = on_attach,
-			root_dir = lspconfig.util.root_pattern("*.js"),
-		})
-
-		--[[ require("mason-lspconfig").setup_handlers {
-			-- The first entry (without a key) will be the default handler
-			-- and will be called for each installed server that doesn't have
-			-- a dedicated handler.
-			function(server_name) -- Default handler (Optional)
-				require("lspconfig")[server_name].setup {
-					capabilities = capabilities,
-					on_attach = on_attach
-				}
-			end
-		} ]]
 	end,
 }
--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -7,5 +7,39 @@ return {
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 		end
+
+		-- Servers Configuration
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local on_attach = function(client, bufnr)
+			require("nvim-navic").attach(client, bufnr)
+		end
+
+		local lspconfig = require("lspconfig")
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = {
+				Lua = {
+					completion = {
+						callSnippet = "Replace",
+					},
+					diagnostics = {
+						-- Get the language server to recognize the `vim` global
+						globals = { "vim" },
+					},
+				},
+			},
+		})
+
+		lspconfig.clangd.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			root_dir = lspconfig.util.root_pattern("*.c", "*.cpp", "*.h", "*.hpp"),
+		})
+
+		lspconfig.tsserver.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 	end,
 }
