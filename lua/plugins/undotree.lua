20fd343634a00d0b9a804419ec068767eb53580f
Nias26 <genangeliludovico26@gmail.com>
2024-04-12 23:31:53 +0200

    fix(undotree): fix keybind not working
    as it was going in contrast with lazy-loading so keymap will be set
    after lazy-loading

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,7 +1,9 @@
 return {
 	"mbbill/undotree",
+	keys = "U",
 	config = function()
 		vim.g.undotree_SetFocusWhenToggle = 1
 		vim.g.undotree_DiffAutoOpen = 0
+		vim.keymap.set("n", "U", "<cmd>UndotreeToggle<CR>", { desc = "Open undo history" })
 	end,
 }
