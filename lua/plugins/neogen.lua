180886b24453643562f31b3704c7ff4bb6f211d5
Nias26 <genangeliludovico26@gmail.com>
2024-04-21 18:54:29 +0200

    reomve(plugin): neogen

--- lua/plugins/neogen.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"danymat/neogen",
-	cmd = "Neogen",
-	dependencies = { "nvim-treesitter/nvim-treesitter" },
-	config = function()
-		require("neogen").setup({ snippet_engine = "luasnip" })
-	end,
-}
