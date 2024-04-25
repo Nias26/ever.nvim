1bf701bd661a15ac577df21bf1c99239b8c471f3
Nias26 <genangeliludovico26@gmail.com>
2024-04-25 15:57:15 +0200

    plugins(bufferline): updated config to main branch

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,24 +1,33 @@
 return {
 	"akinsho/bufferline.nvim",
 	-- WARN:Do not set lazy-loaing on bufferline, or it won't show!
-	tag = "v4.3.0",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		---@diagnostic disable-next-line: missing-fields
-		require("bufferline").setup({
-			---@diagnostic disable-next-line: missing-fields
+		local bufferline = require("bufferline")
+		bufferline.setup({
 			options = {
-				separator_style = "thin",
-				diagnostics = "nvim_lsp",
-				diagnostics_indicator = function(count, level, diagnostics_dict, context)
-					local s = " "
-					for e, n in pairs(diagnostics_dict) do
-						local sym = e == "error" and " " or (e == "warning" and " " or " ")
-						s = s .. n .. sym
-					end
-					return s
+				mode = "buffers",
+				style_preset = bufferline.style_preset.no_italic,
+				themable = true,
+				numbers = "none",
+				buffer_close_icon = "",
+				modified_icon = "●",
+				close_icon = "",
+				diagnostics = false,
+				color_icons = true,
+				get_element_icon = function(element)
+					local icon, hl =
+						require("nvim-web-devicons").get_icon_by_filetype(element.filetype, { default = false })
+					return icon, hl
 				end,
+				show_buffer_icons = true,
+				show_buffer_clone_icons = false,
 				show_close_icon = false,
+				show_tab_indicators = true,
+				show_duplicate_prefix = false,
+				separator_style = "thin",
+				enforce_regular_tabs = true,
+				always_show_bufferline = true,
+				sort_by = "insert_at_end",
 			},
 		})
 	end,
