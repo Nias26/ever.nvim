a71dd9c5e179225ddca7697507645c8842c52c29
Nias26 <genangeliludovico26@gmail.com>
2024-06-04 19:40:54 +0200

    plugins(lualine)!: Rewritten config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -42,8 +42,9 @@ return {
 		local config = {
 			options = {
 				-- Disable sections and component separators
-				component_separators = "",
+				component_separators = " ",
 				section_separators = "",
+				-- theme = "auto",
 				theme = {
 					-- We are going to use lualine_c an lualine_x as left and
 					-- right section. Both are highlighted by c theme .  So we
@@ -53,17 +54,180 @@ return {
 				},
 			},
 			sections = {
-				-- these are to remove the defaults
 				lualine_a = {},
 				lualine_b = {},
-				lualine_x = {},
 				lualine_y = {},
-				-- These will be filled later
-				lualine_c = {},
 				lualine_z = {},
+
+				lualine_c = {
+					{
+						-- mode component
+						function()
+							local mode = vim.api.nvim_get_mode().mode
+							local mode_name
+
+							if mode == "n" or mode == "no" then
+								mode_name = "RW"
+							elseif mode == "i" or mode == "ic" then
+								mode_name = "**"
+							elseif mode == "v" or mode == "V" or mode == "\022" then
+								mode_name = "**"
+							elseif mode == "R" then
+								mode_name = "RA"
+							elseif mode == "c" then
+								mode_name = "VIEX"
+							elseif mode == "t" then
+								mode_name = ""
+							end
+							return " " .. mode_name .. " "
+						end,
+						color = function()
+							-- auto change color according to neovims mode
+							local mode = vim.api.nvim_get_mode().mode
+
+							if mode == "n" or mode == "no" then
+								return { bg = "#82cfff", fg = colors.bg }
+							elseif mode == "i" or mode == "ic" then
+								return { bg = "#ff7eb6", fg = colors.bg }
+							elseif mode == "v" or mode == "V" or mode == "\022" then
+								return { bg = "#be95ff", fg = colors.bg }
+							elseif mode == "R" then
+								return { bg = "#3ddbd9", fg = colors.bg }
+							elseif mode == "c" then
+								return { bg = "#42be65", fg = colors.bg }
+							elseif mode == "t" then
+								return { bg = "#33b1ff", fg = colors.bg }
+							end
+						end,
+						padding = { rignt = 1 },
+					},
+					{
+						"branch",
+						icon = "",
+						color = { fg = colors.orange, gui = "bold" },
+						cond = conditions.check_git_workspace,
+					},
+					{
+						"diff",
+						cond = conditions.check_git_workspace,
+					},
+					{
+						"diagnostics",
+						sources = { "nvim_diagnostic" },
+						symbols = { error = " ", warn = " ", info = " " },
+						diagnostics_color = {
+							color_error = { fg = colors.red },
+							color_warn = { fg = colors.yellow },
+							color_info = { fg = colors.cyan },
+						},
+					},
+				},
+				lualine_x = {
+					{
+						require("noice").api.status.search.get,
+						cond = require("noice").api.status.search.has,
+						icon = " ",
+						color = { fg = colors.blue },
+					},
+					{
+						require("noice").api.status.mode.get,
+						cond = require("noice").api.status.mode.has,
+						color = { fg = colors.green },
+					},
+					{
+						require("noice").api.status.command.get,
+						cond = require("noice").api.status.command.has,
+						icon = "",
+						color = { fg = colors.green },
+					},
+					{
+						"filetype",
+						-- fmt = string.upper,
+						icons_enabled = true,
+						color = { fg = colors.green, gui = "bold" },
+					},
+					{
+						"o:encoding", -- option component same as &encoding in viml
+						fmt = string.upper, -- I'm not sure why it's upper case either ;
+						color = { fg = colors.green, gui = "bold" },
+					},
+					{
+						"location",
+						color = { fg = colors.fg, gui = "bold" },
+					},
+					{
+						function()
+							local chars = setmetatable({
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+								" ",
+							}, {
+								__index = function()
+									return " "
+								end,
+							})
+							local line_ratio = vim.api.nvim_win_get_cursor(0)[1] / vim.api.nvim_buf_line_count(0)
+							local position = math.floor(line_ratio * 100)
+
+							local icon = chars[math.floor(line_ratio * #chars)] .. position
+							if position <= 5 then
+								icon = " TOP"
+							elseif position >= 95 then
+								icon = " BOT"
+							end
+							return icon
+						end,
+						color = function()
+							local position =
+								math.floor(vim.api.nvim_win_get_cursor(0)[1] / vim.api.nvim_buf_line_count(0) * 100)
+							local fg
+							local style
+
+							if position <= 5 then
+								fg = colors.blue
+								style = "bold"
+							elseif position >= 95 then
+								fg = colors.red
+								style = "bold"
+							else
+								fg = colors.violet
+								style = nil
+							end
+							return {
+								fg = fg,
+								style = style,
+								bg = "bg",
+							}
+						end,
+					},
+				},
 			},
 			inactive_sections = {
-				-- these are to remove the defaults
 				lualine_a = {},
 				lualine_b = {},
 				lualine_c = {},
@@ -72,227 +236,6 @@ return {
 				lualine_z = {},
 			},
 		}
-
-		-- Inserts a component in lualine_c at left section
-		local function ins_left(component)
-			table.insert(config.sections.lualine_c, component)
-		end
-
-		-- Inserts a component in lualine_x at right section
-		local function ins_right(component)
-			table.insert(config.sections.lualine_z, component)
-		end
-
-		ins_left({
-			-- mode component
-			function()
-				local mode = vim.api.nvim_get_mode().mode
-				local mode_name
-
-				if mode == "n" or mode == "no" then
-					mode_name = "RW"
-				elseif mode == "i" or mode == "ic" then
-					mode_name = "**"
-				elseif mode == "v" or mode == "V" or mode == "\022" then
-					mode_name = "**"
-				elseif mode == "R" then
-					mode_name = "RA"
-				elseif mode == "c" then
-					mode_name = "VIEX"
-				elseif mode == "t" then
-					mode_name = ""
-				end
-				return " " .. mode_name
-			end,
-			color = function()
-				-- auto change color according to neovims mode
-				local mode = vim.api.nvim_get_mode().mode
-
-				if mode == "n" or mode == "no" then
-					return { bg = "#82cfff", fg = colors.bg }
-				elseif mode == "i" or mode == "ic" then
-					return { bg = "#ff7eb6", fg = colors.bg }
-				elseif mode == "v" or mode == "V" or mode == "\022" then
-					return { bg = "#be95ff", fg = colors.bg }
-				elseif mode == "R" then
-					return { bg = "#3ddbd9", fg = colors.bg }
-				elseif mode == "c" then
-					return { bg = "#42be65", fg = colors.bg }
-				elseif mode == "t" then
-					return { bg = "#33b1ff", fg = colors.bg }
-				end
-			end,
-			padding = { right = 1 },
-		})
-
-		ins_left({
-			"branch",
-			icon = "",
-			color = { fg = colors.violet, gui = "bold" },
-			cond = conditions.check_git_workspace,
-		})
-
-		ins_left({
-			"diff",
-			symbols = { added = "󰐕 ", modified = "󰏪 ", removed = "󰍴 " },
-			diff_color = {
-				added = { fg = colors.green },
-				modified = { fg = colors.orange },
-				removed = { fg = colors.red },
-			},
-			cond = conditions.check_git_workspace,
-		})
-
-		-- Insert mid section. You can make any number of sections in neovim :)
-		-- for lualine it's any number greater then 2
-		ins_left({
-			function()
-				if not conditions.check_git_workspace() then
-					return "                    %="
-				else
-					return "%="
-				end
-			end,
-		})
-
-		ins_left({
-			function()
-				return require("nvim-treesitter").statusline({
-					indicator_size = 70,
-					type_patterns = { "function", "method" },
-					separator = "  ",
-				})
-			end,
-			icon = "󰊕:",
-			color = { fg = "#7cb518", gui = "bold" },
-		})
-
-		ins_left({
-			"diagnostics%=",
-			sources = { "nvim_diagnostic" },
-			symbols = { error = " ", warn = " ", info = " " },
-			diagnostics_color = {
-				color_error = { fg = colors.red },
-				color_warn = { fg = colors.yellow },
-				color_info = { fg = colors.cyan },
-			},
-		})
-
-		-- Add components to right sections
-
-		ins_right({
-			require("noice").api.status.search.get,
-			cond = require("noice").api.status.search.has,
-			icon = " ",
-			color = { fg = colors.blue },
-		})
-
-		ins_right({
-			require("noice").api.status.mode.get,
-			cond = require("noice").api.status.mode.has,
-			color = { fg = "#ff9e64" },
-		})
-
-		ins_right({
-			require("noice").api.status.command.get,
-			cond = require("noice").api.status.command.has,
-			icon = "",
-			color = { fg = "#ff9e64" },
-		})
-
-		ins_right({
-			"filetype",
-			-- fmt = string.upper,
-			icons_enabled = true,
-			color = { fg = colors.green, gui = "bold" },
-		})
-
-		ins_right({
-			"o:encoding", -- option component same as &encoding in viml
-			fmt = string.upper, -- I'm not sure why it's upper case either ;)
-			color = { fg = colors.green, gui = "bold" },
-		})
-
-		ins_right({ "location", color = { fg = colors.fg, gui = "bold" } })
-
-		ins_right({
-			function()
-				local chars = setmetatable({
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-				}, {
-					__index = function()
-						return " "
-					end,
-				})
-				local line_ratio = vim.api.nvim_win_get_cursor(0)[1] / vim.api.nvim_buf_line_count(0)
-				local position = math.floor(line_ratio * 100)
-
-				local icon = chars[math.floor(line_ratio * #chars)] .. position
-				if position <= 5 then
-					icon = " TOP"
-				elseif position >= 95 then
-					icon = " BOT"
-				end
-				return icon
-			end,
-			color = function()
-				local position = math.floor(vim.api.nvim_win_get_cursor(0)[1] / vim.api.nvim_buf_line_count(0) * 100)
-				local fg
-				local style
-
-				if position <= 5 then
-					fg = colors.blue
-					style = "bold"
-				elseif position >= 95 then
-					fg = colors.red
-					style = "bold"
-				else
-					fg = colors.violet
-					style = nil
-				end
-				return {
-					fg = fg,
-					style = style,
-					bg = "bg",
-				}
-			end,
-		})
-
-		-- WARN: If this component is not here, all lualine will fail to load???
-		ins_right({
-			function()
-				return "▊"
-			end,
-			color = { fg = colors.bg },
-			padding = { left = -1 },
-		})
 
 		-- Now don't forget to initialize lualine
 		lualine.setup(config)
