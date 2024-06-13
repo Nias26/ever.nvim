2ede8e33f7fde4cfb8eaa54c13d4a1f2bcd2f0ad
Nias26 <genangeliludovico26@gmail.com>
2024-06-13 13:23:54 +0200

    plugins(rm): removed unused plugins
    - git-blame.nvim (unused)
    - notifier.nvim (using builtin noice notifier)
    - nvim-docs-view.nvim (unused)

--- lua/plugins/notifier.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"vigoux/notifier.nvim",
-	event = "VeryLazy",
-	config = function()
-		require("notifier").setup({ components = {} })
-		require("notifier.config").config.components = { "nvim", "lsp" }
-	end,
-}
