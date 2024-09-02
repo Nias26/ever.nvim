16de7409a77fb589aa5940d5fb9c5d27e18b637a
Nias26 <genangeliludovico26@gmail.com>
2024-09-02 20:05:12 +0200

    style(toggleterm): changed direction from 'horizziontal' to 'vertical'

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,14 +3,20 @@ return {
 	keys = "<F4>",
 	version = "*",
 	opts = {
-		size = 20,
+		size = function(term)
+			if term.direction == "horizontal" then
+				return 15
+			elseif term.direction == "vertical" then
+				return vim.o.columns * 0.4
+			end
+		end,
 		open_mapping = "<F4>",
 		hide_numbers = true,
 		shade_filetypes = {},
 		shade_terminals = true,
 		start_in_insert = true,
 		persist_size = true,
-		direction = "horizontal",
+		direction = "vertical",
 		close_on_exit = true,
 		float_opts = {
 			border = "curved",
