d4098a5d34ca302d13055323ce1aec691c900bb4
Nias26 <genangeliludovico26@gmail.com>
2024-09-04 11:33:35 +0200

    style: format

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -5,16 +5,16 @@ return {
 		local builtin = require("statuscol.builtin")
 		require("statuscol").setup({
 			segments = {
-				{ text = { "%s" },             click = "v:lua.ScSa" },
-				{ text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
+				{ text = { "%s" }, click = "v:lua.ScSa" },
+				{ text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
 				{
 					text = { " ", builtin.foldfunc, " " },
 					condition = { builtin.not_empty, true, builtin.not_empty },
-					click = "v:lua.ScFa"
+					click = "v:lua.ScFa",
 				},
 			},
 			ft_ignore = { "NvimTree", "dashboard" },
-			bt_ignore = { "NvimTree", "dashboard" }
+			bt_ignore = { "NvimTree", "dashboard" },
 		})
-	end
+	end,
 }
