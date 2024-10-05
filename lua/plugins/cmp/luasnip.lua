a80a1bf3db362384c5030e69ee4d1310e1d8c4e1
Nias26 <genangeliludovico26@gmail.com>
2024-10-06 00:15:53 +0200

    ci(luasnip): build jsregexp for snippets transformation

--- lua/plugins/cmp/luasnip.lua
+++ lua/plugins/cmp/luasnip.lua
@@ -2,6 +2,7 @@ return {
 	"L3MON4D3/LuaSnip",
 	event = "InsertEnter",
 	dependencies = { "rafamadriz/friendly-snippets" },
+	run = { "make install_jsregexp" },
 	config = function()
 		require("luasnip.loaders.from_vscode").lazy_load()
 	end,
