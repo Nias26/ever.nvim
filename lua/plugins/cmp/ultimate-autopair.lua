dcb95576eefd615a3695905d8db9cc47095e06bf
Nias26 <genangeliludovico26@gmail.com>
2024-11-01 11:41:44 +0100

    ci(autopair): removed unecessary functionality

--- lua/plugins/cmp/ultimate-autopair.lua
+++ lua/plugins/cmp/ultimate-autopair.lua
@@ -121,7 +121,7 @@ return {
 				--contains extension config
 				multi = false,
 				--use multiple configs (|ultimate-autopair-map-multi-config|)
-				hopout = true,
+				hopout = false,
 				-- (|) > tabout > ()|
 				do_nothing_if_fail = true,
 				--add a module so that if close fails
