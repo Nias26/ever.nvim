9d7c89b05ea9acd17ea2e7facb5c175083962666
Nias26 <genangeliludovico26@gmail.com>
2025-01-16 19:25:20 +0100

    style(format): format

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -4,10 +4,17 @@ local map = vim.keymap.set
 -- Movment
 map({ "n", "v" }, "H", "<S-Left>", { desc = "Move 1 word to the left" })
 map({ "n", "v" }, "L", "<S-Right>", { desc = "Move 1 word to the right" })
--- Cancel highlightning when searching
-map("n", "<ESC><ESC>", "<cmd>nohlsearch<CR>")
--- Fast switch window
+map("n", "<C-h>", "<C-w>h", { desc = "Go to the left window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to the left window" })
+map("n", "<C-Left>", "<C-w>h", { desc = "Go to the left window" })
+map("n", "<C-Right>", "<C-w>l", { desc = "Go to the right window" })
+map("n", "<C-w><", "<C-w><<C-w><<C-w><<C-w><", { desc = "Decrease width" })
+map("n", "<C-w>>", "<C-w>><C-w>><C-w>><C-w>>", { desc = "Increase width" })
 map("n", "!", "<C-w>w")
+
+-- Remove highlightning
+map("n", "<ESC><ESC>", "<cmd>nohlsearch<CR>")
+
 -- Open Telescope
 map("n", ";;", "<cmd>Telescope<CR>", { desc = "Telescope" })
 map("n", ";G", "<cmd>Telescope live_grep<CR>", { desc = "Live Grep" })
@@ -17,16 +24,20 @@ map("n", ";s", "<cmd>Telescope file_browser<CR>", { desc = "File Browser" })
 map("n", ";b", "<cmd>Telescope buffers<CR>", { desc = "Buffers" })
 map("n", ";h", "<cmd>Telescope help_tags<CR>", { desc = "Help Tags" })
 map("n", ";m", "<cmd>Telescope man_pages<CR>", { desc = "Man Pages" })
+
 -- Open Nvim Tree
 map("n", "<F3>", "<cmd>NvimTreeToggle<CR>", { desc = "Open Nvim Tree" })
+
 -- Buffer managing
 map("n", "bd", "<cmd>bdel<CR>", { desc = "Delete current buffer" })
 map("n", "<Tab>", "<cmd>bnext<CR>", { desc = "Previous buffer" })
 map("n", "<S-Tab>", "<cmd>bprevious<CR>", { desc = "Next buffer" })
+
 -- Splits
 map("n", "Ss", ":split ", { desc = "Horizzontal Split" })
 map("n", "Sv", ":vsplit ", { desc = "Vertical Split" })
 map({ "n", "v" }, "Q", "<cmd>bd!<CR>", { desc = "Quit current buffer" })
+
 -- LSP
 map({ "n", "v" }, "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
 map({ "n", "v" }, "gr", vim.lsp.buf.references, { desc = "Go to Reference" })
@@ -39,10 +50,10 @@ map("n", "cd", "<cmd>Lspsaga hover_doc<CR>", { desc = "LSP Code Doc" })
 map("n", "cr", vim.lsp.buf.rename, { desc = "LSP Rename" })
 map("n", "c[", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { desc = "Go to Previous Diagnostic" })
 map("n", "c]", "<cmd>Lspsaga diagnostic_jump_next<CR>", { desc = "Go to Next Diagnostic" })
--- Special paste
-map("v", "P", '"_dP', { desc = "Delete and paste" })
+
 -- Undotree
 map("n", "U", "<cmd>UndotreeToggle<CR>", { desc = "Open undo history" })
+
 -- Indent whole page
 map("n", "==", function()
 	local c = vim.fn.getpos(".")
@@ -51,6 +62,7 @@ map("n", "==", function()
 	vim.fn.setpos(".", c)
 	vim.fn.winrestview(v)
 end, { desc = "Indent whole page" })
+
 -- Grug-far
 map("n", ";r", function()
 	require("grug-far").open({
@@ -59,6 +71,7 @@ map("n", ";r", function()
 		},
 	})
 end, { desc = "Grug-Far" })
+
 -- Toggle relativenumbers
 map("n", "<F5>", function()
 	vim.wo.rnu = not vim.wo.rnu
