775b8d546d6d7f8138cb9e15ad39705ca131b10b
Nias26 <genangeliludovico26@gmail.com>
2025-01-17 17:46:27 +0100

    ci(statuscol): update config

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -4,14 +4,32 @@ return {
 	config = function()
 		local builtin = require("statuscol.builtin")
 		require("statuscol").setup({
+			thousands = false,
+			relculright = false,
 			segments = {
 				{ text = { "%s" }, click = "v:lua.ScSa" },
-				{ text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+				{
+					text = { builtin.lnumfunc, " " },
+					condition = { true, builtin.not_empty },
+					click = "v:lua.ScLa",
+				},
 				{
 					text = { " ", builtin.foldfunc, " " },
 					condition = { builtin.not_empty, true, builtin.not_empty },
 					click = "v:lua.ScFa",
 				},
+			},
+			clickmod = "c",
+			clickhandlers = {
+				Lnum = builtin.lnum_click,
+				FoldClose = builtin.foldclose_click,
+				FoldOpen = builtin.foldopen_click,
+				FoldOther = builtin.foldother_click,
+				DapBreakpointRejected = builtin.toggle_breakpoint,
+				DapBreakpoint = builtin.toggle_breakpoint,
+				DapBreakpointCondition = builtin.toggle_breakpoint,
+				["diagnostic/signs"] = builtin.diagnostic_click,
+				gitsigns = builtin.gitsigns_click,
 			},
 			ft_ignore = { "NvimTree", "dashboard" },
 			bt_ignore = { "NvimTree", "dashboard" },
