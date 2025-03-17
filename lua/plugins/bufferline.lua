48038db901a11a2997ea7c3f7c001f2fd38084af
Nias26 <genangeliludovico26@gmail.com>
2025-03-17 22:47:20 +0100

    ci(buf/tabline): adjusted buffer and tabline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,41 +2,72 @@ return {
 	"akinsho/bufferline.nvim",
 	dependencies = {
 		{
+			"nvim-tree/nvim-web-devicons",
+		},
+		{
 			"tiagovla/scope.nvim",
 			config = function()
 				require("scope").setup()
 			end,
 		},
 	},
-	-- WARN:Do not set lazy-loaing, or it won't load properly
 	config = function()
-		local bufferline = require("bufferline")
-		bufferline.setup({
+		local config = {
 			options = {
-				mode = "buffers",
-				style_preset = bufferline.style_preset.no_italic,
-				themable = true,
+				mode = "tabs", -- set to "tabs" to only show tabpages instead
+				style_preset = require("bufferline").style_preset.no_italic,
 				numbers = "none",
-				buffer_close_icon = "",
-				modified_icon = "●",
-				close_icon = "",
+				indicator = {
+					icon = "▎", -- this should be omitted if indicator style is not 'icon'
+					style = "icon",
+				},
+				truncate_names = true, -- whether or not tab names should be truncated
+				tab_size = 18,
 				diagnostics = false,
-				color_icons = true,
+				custom_filter = function(buf)
+					return false -- Show only tabs
+				end,
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "Files",
+						text_align = "center",
+						separator = true,
+					},
+				},
+				color_icons = true, -- whether or not to add the filetype icon highlights
 				get_element_icon = function(element)
+					-- element consists of {filetype: string, path: string, extension: string, directory: string}
+					-- This can be used to change how bufferline fetches the icon
+					-- for an element e.g. a buffer or a tab.
+					-- e.g.
 					local icon, hl =
 						require("nvim-web-devicons").get_icon_by_filetype(element.filetype, { default = false })
 					return icon, hl
 				end,
-				show_buffer_icons = true,
-				show_buffer_clone_icons = false,
+				show_buffer_icons = true, -- disable filetype icons for buffers
+				show_buffer_close_icons = false,
 				show_close_icon = false,
 				show_tab_indicators = true,
-				show_duplicate_prefix = false,
+				show_duplicate_prefix = false, -- whether to show duplicate buffer prefix
+				-- can also be a table containing 2 custom separators
+				-- [focused and unfocused]. eg: { '|', '|' }
 				separator_style = "thin",
 				enforce_regular_tabs = true,
-				always_show_bufferline = true,
-				sort_by = "insert_at_end",
+				always_show_bufferline = false,
 			},
+		}
+
+		-- Lazy loading
+		vim.api.nvim_create_autocmd({ "BufAdd", "TabEnter" }, {
+			pattern = "*",
+			group = vim.api.nvim_create_augroup("BufferLineLazyLoading", {}),
+			callback = function()
+				local count = #vim.fn.gettabinfo()
+				if count >= 2 then
+					require("bufferline").setup(config)
+				end
+			end,
 		})
 	end,
 }
