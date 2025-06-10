9b9dbfbfa41609d67c3a0c90b874407a76b5cfe5
Nias26 <genangeliludovico26@gmail.com>
2025-06-10 12:46:24 +0200

    feat(snip): added c++ header guard snippet
    the one coming from friendly snippets - vscode sucks ass

--- lua/plugins/cmp/luasnip.lua
+++ lua/plugins/cmp/luasnip.lua
@@ -5,5 +5,25 @@ return {
 	run = { "make install_jsregexp" },
 	config = function()
 		require("luasnip.loaders.from_vscode").lazy_load()
+
+		local ls = require("luasnip")
+		local s = ls.snippet
+		local t = ls.text_node
+		local i = ls.insert_node
+		local extras = require("luasnip.extras")
+		local rep = extras.rep
+
+    -- stylua: ignore
+		ls.add_snippets("cpp" , {
+			s(
+				"guard",{
+          t("#ifndef H_"), i(1),
+          t({"", "#define H_"}), rep(1),
+          t({"", ""}),
+          t({"", ""}),
+          t({"", "#endif // !"}), rep(1),
+        }
+			),
+		})
 	end,
 }
