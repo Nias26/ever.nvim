3fc856f9361cf6ed9a409436b3b44245b886c2cb
Nias26 <genangeliludovico26@gmail.com>
2025-07-17 13:19:42 +0200

    feat(ibl): add animation scope

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,48 +1,57 @@
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	event = "BufRead",
-	name = "ibl",
-	config = function()
-		require("ibl").setup({
-			exclude = {
-				buftypes = {
-					"alpha",
-					"dashboard",
-					"lspinfo",
-					"lazy",
-					"checkhealth",
-					"help",
-					"man",
-					"Terminal",
-					"TelescopePrompt",
-					"TelescopeResults",
-					"mason",
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = "BufRead",
+		name = "ibl",
+		config = function()
+			require("ibl").setup({
+				exclude = {
+					buftypes = {
+						"alpha",
+						"dashboard",
+						"lspinfo",
+						"lazy",
+						"checkhealth",
+						"help",
+						"man",
+						"Terminal",
+						"TelescopePrompt",
+						"TelescopeResults",
+						"mason",
+					},
+					filetypes = {
+						"alpha",
+						"dashboard",
+						"lspinfo",
+						"lazy",
+						"checkhealth",
+						"help",
+						"man",
+						"Terminal",
+						"TelescopePrompt",
+						"TelescopeResults",
+						"mason",
+					},
 				},
-				filetypes = {
-					"alpha",
-					"dashboard",
-					"lspinfo",
-					"lazy",
-					"checkhealth",
-					"help",
-					"man",
-					"Terminal",
-					"TelescopePrompt",
-					"TelescopeResults",
-					"mason",
+				indent = {
+					highlight = { "Whitespace" },
+					char = "|",
+					smart_indent_cap = true,
 				},
-			},
-			indent = {
-				highlight = { "Whitespace" },
-				char = "|",
-				smart_indent_cap = true,
-			},
-			scope = {
-				enabled = true,
-				char = "|",
-				show_exact_scope = true,
-				injected_languages = true,
-			},
-		})
-	end,
+				scope = {
+					enabled = true,
+					char = "|",
+					show_exact_scope = true,
+					injected_languages = true,
+				},
+			})
+		end,
+	},
+	{
+		"echasnovski/mini.indentscope",
+		version = "*",
+		opts = {
+			symbol = "|",
+		},
+	},
 }
