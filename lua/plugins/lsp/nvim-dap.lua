eb8c06cc2b9e5fff8c1c3802dc1bc3e887429ddf
Nias26 <genangeliludovico26@gmail.com>
2025-08-27 13:56:00 +0200

    style(dap): change highlight groups to match oxocarbon

--- lua/plugins/lsp/nvim-dap.lua
+++ lua/plugins/lsp/nvim-dap.lua
@@ -71,5 +71,69 @@ return {
 		vim.keymap.set("n", "<localleader>O", "<cmd>DapStepOut<CR>", { desc = "(DAP) Step Out" })
 		vim.keymap.set("n", "<localleader>r", "<cmd>DapRestartFrame<CR>", { desc = "(DAP) Restart Session" })
 		vim.keymap.set("n", "<localleader>t", "<cmd>DapTerminate<CR>", { desc = "(DAP) Terminate Session" })
+
+		local oxocarbon = {
+			base00 = "#161616",
+			base01 = "#262626",
+			base02 = "#393939",
+			base03 = "#525252",
+			base04 = "#dde1e6",
+			base05 = "#f2f4f8",
+			base06 = "#ffffff",
+			base07 = "#08bdba",
+			base08 = "#3ddbd9",
+			base09 = "#78a9ff",
+			base0A = "#ee5396",
+			base0B = "#33b1ff",
+			base0C = "#ff7eb6",
+			base0D = "#42be65",
+			base0E = "#be95ff",
+			base0F = "#82cfff",
+		}
+
+		local hl = vim.api.nvim_set_hl
+		hl(0, "DapUIBreakpointsCurrentLine", { fg = oxocarbon.base0A })
+		hl(0, "DapUIBreakpointsDisabledLine", { fg = oxocarbon.base02 })
+		hl(0, "DapUIBreakpointsInfo", { fg = oxocarbon.base08 })
+		hl(0, "DapUIBreakpointsLine", { fg = oxocarbon.base08 })
+		hl(0, "DapUIBreakpointsPath", { fg = oxocarbon.base07 })
+		hl(0, "DapUICurrentFrameName", { fg = oxocarbon.base0D })
+		-- hl(0, "DapUIDecoration" )
+		-- hl(0, "DapUIEndofBuffer")
+		-- hl(0, "DapUIFloatBorder" )
+		-- hl(0, "DapUIFloatNormal" )
+		-- hl(0, "DapUIFloatNormalNC" )
+		hl(0, "DapUIFrameName", { fg = oxocarbon.base0D })
+		hl(0, "DapUILineNumber", { fg = oxocarbon.base0A })
+		hl(0, "DapUIModifiedValue", { fg = oxocarbon.base09 })
+		-- hl(0, "DapUINormal" )
+		-- hl(0, "DapUINormalNC" )
+		hl(0, "DapUIPlayPause", { fg = oxocarbon.base0D })
+		hl(0, "DapUIPlayPauseNC", { fg = oxocarbon.base0D })
+		hl(0, "DapUIRestart", { fg = oxocarbon.base0D })
+		hl(0, "DapUIRestartNC", { fg = oxocarbon.base0D })
+		hl(0, "DapUIScope", { fg = oxocarbon.base07 })
+		hl(0, "DapUISource", { fg = oxocarbon.base07 })
+		hl(0, "DapUIStepBack", { fg = oxocarbon.base08 })
+		hl(0, "DapUIStepBackNC", { fg = oxocarbon.base08 })
+		hl(0, "DapUIStepInto", { fg = oxocarbon.base08 })
+		hl(0, "DapUIStepIntoNC", { fg = oxocarbon.base08 })
+		hl(0, "DapUIStepOut", { fg = oxocarbon.base08 })
+		hl(0, "DapUIStepOutNC", { fg = oxocarbon.base08 })
+		hl(0, "DapUIStepOver", { fg = oxocarbon.base08 })
+		hl(0, "DapUIStepOverNC", { fg = oxocarbon.base08 })
+		hl(0, "DapUIStop", { fg = oxocarbon.base0A })
+		hl(0, "DapUIStopNC", { fg = oxocarbon.base0A })
+		hl(0, "DapUIStoppedThread", { fg = oxocarbon.base0A })
+		hl(0, "DapUIThread", { fg = oxocarbon.base0D })
+		hl(0, "DapUIType", { fg = oxocarbon.base0E })
+		hl(0, "DapUIUnavailable", { fg = oxocarbon.base02 })
+		hl(0, "DapUIUnavailableNC", { fg = oxocarbon.base02 })
+		hl(0, "DapUIValue", { fg = oxocarbon.base04 })
+		hl(0, "DapUIVariable", { fg = oxocarbon.base04 })
+		-- hl(0, "DapUIWatchesEmpty" )
+		-- hl(0, "DapUIWatchesError" )
+		-- hl(0, "DapUIWatchesValue" )
+		-- hl(0, "DapUIWinSelect" )
 	end,
 }
