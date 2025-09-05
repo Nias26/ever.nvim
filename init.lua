dc3b62a6c6bd4c4d63c41f50576c7a892bd37f53
Nias26 <genangeliludovico26@gmail.com>
2025-09-05 14:24:07 +0200

    Main file

--- init.lua
+++ init.lua
@@ -1,57 +1,35 @@
--- Init.lua file (Main loaded lua file)
+vim.loader.enable()
 
-vim.loader.enable() -- Faster boot time
-vim.g.mapleader = " " -- Set leader key
-vim.g.maplocalleader = "," -- Set localleader
+vim.g.mapleader = " "
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.termguicolors = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to down window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to up window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+vim.keymap.set("n", "Q", "<cmd>bdel<CR>", { desc = "Close buffer" })
+vim.keymap.set("n", "<leader>o", "<cmd>Oil<CR>", { desc = "Oil" }) 
+vim.keymap.set({"n", "v"}, "cc", "gcc", { desc = "Comment line", noremap = true})
+vim.keymap.set("n", "<Tab>", "<cmd>bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
 
--- Lazy.nvim setup
-require("lazy").setup({
-	-- Core Files
-	{ import = "plugins" },
-	{ import = "plugins.lsp" },
-	{ import = "plugins.cmp" },
-	-- Lazy.nvim Opts
-	ui = {
-		icons = {
-			cmd = " ",
-			config = " ",
-			event = "",
-			ft = " ",
-			init = " ",
-			import = " ",
-			keys = " ",
-			lazy = "󰒲 ",
-			loaded = "●",
-			not_loaded = "○",
-			plugin = " ",
-			runtime = " ",
-			require = "󰢱 ",
-			source = " ",
-			start = "",
-			task = "✔ ",
-			list = {
-				"●",
-				"➜ ",
-				"★ ",
-				"‒",
-			},
-		},
-	},
-	concurrency = 5,
+vim.pack.add({
+	{ src = "https://github.com/nyoom-engineering/oxocarbon.nvim" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "" },
+	{ src = "https://github.com/echasnovski/mini.icons" }, -- Oil.nvim dependency
+	{ src = "https://github.com/stevearc/oil.nvim" },	
 })
 
-require("config")
+require("mini.icons").setup()
+require("oil").setup()
+
+vim.cmd.colorscheme("oxocarbon")
+
+vim.lsp.enable("lua_ls", "clangd")
