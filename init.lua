ee043f8d6dede9f60a11aafdf61a3e62df20e532
Nias26 <genangeliludovico26@gmail.com>
2025-09-05 15:44:26 +0200

    fix: format save

--- init.lua
+++ init.lua
@@ -112,6 +112,6 @@ vim.lsp.config("clangd", {
 
 vim.api.nvim_create_autocmd("BufWritePre", {
 	callback = function()
-		vim.lsp.buf.format({ async = true })
+		vim.lsp.buf.format({ async = false })
 	end
 })
