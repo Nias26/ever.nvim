1ddc1f52c38621b7f3a24211c71bc319ed105b1e
Nias26 <genangeliludovico26@gmail.com>
2025-09-30 18:06:48 +0200

    feat(opts): disable unused providers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,14 @@ vim.g.loaded_netrw = 1          -- Disable netrw
 vim.g.loaded_netrwPlugin = 1    -- Disable netrw
 vim.g.showcmdloc = "statusline" -- Show command message location
 
+-- Disable language provider support (lua and vimscript plugins only)
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python_provider = 0
+vim.g.loaded_python3_provider = 0
+
+
 vim.opt.sessionoptions = {      -- Required by scope.nvim
   "buffers",
   "tabpages",
