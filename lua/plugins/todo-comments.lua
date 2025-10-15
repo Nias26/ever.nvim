7155bc4d3d72e5bfd8ee8d3bb8cb9dd83ca24ff5
Nias26 <genangeliludovico26@gmail.com>
2025-10-15 11:43:18 +0200

    feat(comments): configure setup

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -2,5 +2,19 @@ return {
 	"folke/todo-comments.nvim",
 	event = "BufRead",
 	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = {},
+	config = function()
+		require("todo-comments").setup({
+			keywords = {
+				DONE = { icon = " ", color = "success" },
+				HERE = { icon = " ", color = "error" },
+				TEST = { icon = "⏲ ", color = "test", alt = nil },
+				TESTING = { icon = "⏲ ", color = "warning" },
+				PASSED = { icon = " ", color = "success" },
+				FAILED = { icon = " ", color = "error" },
+			},
+			colors = {
+				success = { "#42be65" },
+			},
+		})
+	end,
 }
