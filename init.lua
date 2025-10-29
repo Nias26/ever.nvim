0acb5d6d7d28487410cd395c00ec13884b6b0ec5
Nias26 <genangeliludovico26@gmail.com>
2025-10-29 21:39:29 +0100

    fix: lsp hover

--- init.lua
+++ init.lua
@@ -134,12 +134,6 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 vim.api.nvim_create_autocmd("CursorHold", {
 	pattern = "*.*",
 	callback = function()
-		for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
-			if vim.api.nvim_win_get_config(winid).zindex then
-				return
-			end
-		end
-
 		vim.diagnostic.open_float(0, {
 			scope = "cursor",
 			focusable = false,
