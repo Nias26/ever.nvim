829547f56e0a34431a28f608d370410a37e5fce9
Nias26 <genangeliludovico26@gmail.com>
2025-12-18 14:00:46 +0100

    fix: error in treesitter setup

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -4,7 +4,7 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		---@diagnostic disable-next-line: missing-fields
-		require("nvim-treesitter.configs").setup({
+		require("nvim-treesitter").setup({
 			-- A list of parser names, or "all" (the five listed parsers should always be installed)
 			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "cpp", "markdown", "markdown_inline", "regex" },
 			-- Install parsers synchronously (only applied to `ensure_installed`)
