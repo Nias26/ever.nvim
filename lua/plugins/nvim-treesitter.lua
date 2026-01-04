7e49358bf5268509c45591f6729936f51c104e4c
Nias26 <genangeliludovico26@gmail.com>
2026-01-04 23:50:39 +0100

    refactor(treesitter): new config

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -6,26 +6,17 @@ return {
 	},
 	build = ":TSUpdate",
 	config = function()
-		---@diagnostic disable-next-line: missing-fields
-		require("nvim-treesitter").setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "regex" },
-			sync_install = false,
-			auto_install = true,
-			ignore_install = {},
-			highlight = {
-				enable = true,
-				disable = function(lang, buf)
-					local max_filesize = 100 * 1024 -- 100 KB
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > max_filesize then
-						return true
-					end
-				end,
-				additional_vim_regex_highlighting = false,
-			},
+		-- require("nvim-treesitter").install({
+		-- 	"c",
+		-- 	"lua",
+		-- 	"vim",
+		-- 	"vimdoc",
+		-- 	"query",
+		-- 	"markdown",
+		-- 	"markdown_inline",
+		-- 	"regex",
+		-- })
 
-			indent = { enable = true },
-		})
 		vim.filetype.add({
 			pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
 		})
