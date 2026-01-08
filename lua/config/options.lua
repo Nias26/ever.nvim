61c37e3f760b1cb87812c4ecdfd6596b38fc5ddf
Nias26 <genangeliludovico26@gmail.com>
2026-01-08 16:20:32 +0100

    feat(opts): don't ask for everything i do

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -67,7 +67,6 @@ local options = {
   gdefault = true,                                                                  -- Always substitute globally
   path = { ".", "**" },                                                             -- Search path
   splitkeep = "screen",                                                             -- Keep text on screen line
-  confirm = true                                                                    -- Ask for confiration on exit
 }
 
 for k, v in pairs(options) do
