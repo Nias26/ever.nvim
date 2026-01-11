7d2fd9c4dfdfae0c192419c05acd30e106c21218
Nias26 <genangeliludovico26@gmail.com>
2026-01-11 10:00:54 +0100

    feat(notify): hide title for notification without one

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -5,7 +5,18 @@ return {
 		---@diagnostic disable-next-line: missing-fields
 		require("notify").setup({
 			background_colour = "#000000",
-			render = "compact",
+			render = function(bufnr, notif, highlights, config)
+				local notify_renderers = require("notify.render")
+				local minimal_titles = { "", "Messages", "Notify" }
+				if vim.list_contains(minimal_titles, notif.title[1]) then
+					highlights.body = "Notify" .. notif.level .. "Title"
+					notif.message[1] = " " .. notif.icon .. "\t" .. notif.message[1]
+
+					return notify_renderers.minimal(bufnr, notif, highlights, config)
+				end
+
+				return notify_renderers.compact(bufnr, notif, highlights, config)
+			end,
 			stages = "static",
 			top_down = false,
 			on_open = function(win)
