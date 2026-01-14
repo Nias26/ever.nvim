cdea2fd18a918b042302d3c78db881a4c1a91ce8
Nias26 <genangeliludovico26@gmail.com>
2026-01-14 13:08:24 +0100

    feat(plugins): add vim-fugitive

---
+++ lua/plugins/vim-fugitive.lua
@@ -0,0 +1,17 @@
+return {
+	"tpope/vim-fugitive",
+	cmd = {
+		"Git",
+		"G",
+		"Gedit",
+		"Gsplit",
+		"Gdiffsplit",
+		"Gvdiffsplit",
+		"Gread",
+		"Gwrite",
+		"Ggrep",
+		"Gmove",
+		"Gdelete",
+		"Gbrowse",
+	},
+}
