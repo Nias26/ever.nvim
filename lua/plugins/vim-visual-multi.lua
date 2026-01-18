a5748144373e946e79bd54c74958e6874d272e1b
Nias26 <genangeliludovico26@gmail.com>
2026-01-18 20:21:27 +0100

    feat(multi): set configuration

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -1,8 +1,23 @@
 return {
 	"mg979/vim-visual-multi",
 	event = "BufEnter",
-	config = function()
+	init = function()
+		vim.g.VM_set_statusline = 0
 		vim.g.VM_mouse_mappings = 0
-		vim.g.VM_maps = {}
+		vim.g.VM_maps = {
+			-- Experimental
+			["Undo"] = "u",
+			["Redo"] = "<C-r>",
+
+			-- Line Cursor
+			["Add Cursor Down"] = "<Down>",
+			["Add Cursor Up"] = "<Up>",
+
+			-- Match Cursor
+			["Find Under"] = "<C-n>",
+			["Find Subword Under"] = "<C-n>",
+
+			["Exit"] = "<Esc>",
+		}
 	end,
 }
