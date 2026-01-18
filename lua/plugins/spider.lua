27a4e9d206459bedbdfa782c17b4c900635794f7
Nias26 <genangeliludovico26@gmail.com>
2026-01-18 11:25:15 +0100

    feat(plugins): add nvim-spider

---
+++ lua/plugins/spider.lua
@@ -0,0 +1,11 @@
+return {
+	"chrisgrieser/nvim-spider",
+	lazy = true,
+	keys = {
+		{ "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "x", "o" } },
+		{ "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "x", "o" } },
+		{ "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "x", "o" } },
+		{ "ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "x", "o" } },
+	},
+	opts = {},
+}
