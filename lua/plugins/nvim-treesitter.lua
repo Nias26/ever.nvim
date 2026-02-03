3a2ae54a960e9eb86327a1ac3171b8f21802c914
Nias26 <genangeliludovico26@gmail.com>
2026-02-03 17:21:05 +0100

    revert: revert treesitter to stable branch

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,22 +1,27 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	event = "VeryLazy",
+	branch = "master",
 	dependencies = {
 		{ "folke/ts-comments.nvim", opts = {} },
 	},
 	build = ":TSUpdate",
 	config = function()
-		-- require("nvim-treesitter").install({
-		-- 	"c",
-		-- 	"lua",
-		-- 	"vim",
-		-- 	"vimdoc",
-		-- 	"query",
-		-- 	"markdown",
-		-- 	"markdown_inline",
-		-- 	"regex",
-		-- })
-
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "regex" },
+			sync_install = false,
+			auto_install = true,
+			highlight = {
+				enable = true,
+				disable = function(lang, buf)
+					local max_filesize = 100 * 1024 -- 100 KB
+					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					if ok and stats and stats.size > max_filesize then
+						return true
+					end
+				end,
+			},
+		})
 		vim.filetype.add({
 			pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
 		})
