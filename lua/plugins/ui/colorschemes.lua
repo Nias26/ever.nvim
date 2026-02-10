2701ac9b0e1e14d4cfdc0081e7e70f9d97c4a012
Nias26 <genangeliludovico26@gmail.com>
2026-02-10 17:26:55 +0100

    refactor: remove unused colorschemes

--- lua/plugins/ui/colorschemes.lua
+++ lua/plugins/ui/colorschemes.lua
@@ -6,50 +6,6 @@ return {
 		end,
 	},
 	{
-		"rebelot/kanagawa.nvim",
-		config = function()
-			require("kanagawa").setup({
-				commentStyle = { italic = false },
-				keywordStyle = { italic = false },
-				dimInactive = true,
-				overrides = function(colors)
-					local theme = colors.theme
-					local palette = colors.palette
-
-					return {
-						--- @highlight Telescope-hl
-						TelescopeTitle = { fg = theme.ui.special, bold = true },
-						TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-						TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-						TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-						TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-						TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-						TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-
-						--- @highlight Line Nubers
-						LineNr = { bg = palette.sumiInk3 },
-						CursorLineNr = { bg = palette.sumiInk3 },
-						--- @highlight Fold Column
-						FoldColumn = { bg = palette.sumiInk3 },
-						--- @highlight Diagnostics
-						SignColumn = { bg = palette.sumiInk3 },
-						DiagnosticSignError = { bg = palette.sumiInk3 },
-						DiagnosticSignWarn = { bg = palette.sumiInk3 },
-						DiagnosticSignHint = { bg = palette.sumiInk3 },
-						DiagnosticSignOk = { bg = palette.sumiInk3 },
-						DiagnosticSignInfo = { bg = palette.sumiInk3 },
-						---@highlight Pmenu
-						Pmenu = { fg = "#2a2a37", bg = "#292937" },
-						PmenuSel = { bg = "#3f3f48", fg = "NONE" },
-
-						["@variable.builtin"] = { italic = false },
-					}
-				end,
-			})
-			vim.ui.border_style = "none"
-		end,
-	},
-	{
 		"vague-theme/vague.nvim",
 		lazy = false,
 		priority = 1000,
@@ -78,6 +34,8 @@ return {
 				"NeoTreeEndOfBuffer",
 				-- Tabline
 				"TablineHeader",
+				-- Lsp CodeLens
+				"LspCodeLens",
 			})
 
 			-- Exclude groups
