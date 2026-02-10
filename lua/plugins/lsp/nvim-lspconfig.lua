9ae79a5edc36e3978f5ec2c58f509c69f07b424f
Nias26 <genangeliludovico26@gmail.com>
2026-02-10 17:26:21 +0100

    feat(lsp): this motherfucker... lazyloaded lsp
    with minimum startup time (~38ms) compared to (~150ms)

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -1,12 +1,9 @@
 return {
-	-- FIX: Lazyload on file not working. Lsp autocmd wont be triggered if created after file is read.
-	-- LazyVim still manages to load it on file
-	-- Try to get the names from mason-lspconfig and run `vim.lsp.enable` manually
 	"neovim/nvim-lspconfig",
-	-- event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+	event = "VeryLazy", -- We lost a war
 	dependencies = {
-		"mason-org/mason-lspconfig.nvim",
-		"mason-org/mason.nvim",
+		{ "mason-org/mason.nvim", build = ":MasonUpdate", opts = {} },
+		{ "mason-org/mason-lspconfig.nvim", opts = {} },
 	},
 	config = function()
 		vim.diagnostic.config({
@@ -20,24 +17,34 @@ return {
 			},
 		})
 
-		local capabilities = require("blink.cmp").get_lsp_capabilities()
-		local on_attach = function(client, bufnr)
-			if client:supports_method("textDocument/inlayHint") then
-				local excluded_servers = { "lua_ls" }
-				if not vim.tbl_contains(excluded_servers, client.name) then
-					vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-				end
-			end
-
-			if client:supports_method("textDocument/codeLens") then
-				vim.api.nvim_set_hl(0, "LspCodeLens", { bg = "NONE" })
-				vim.lsp.codelens.enable()
-			end
-		end
-
 		vim.lsp.config("*", {
-			capabilities = capabilities,
-			on_attach = on_attach,
+			capabilities = require("blink.cmp").get_lsp_capabilities(),
+			on_attach = function(client, bufnr)
+				if client:supports_method("textDocument/inlayHint") then
+					local excluded = { "lua_ls" }
+					if not vim.tbl_contains(excluded, client.name) then
+						vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+					end
+				end
+
+				if client:supports_method("textDocument/codeLens") then
+					local excluded = {}
+					if not vim.tbl_contains(excluded, client.name) then
+						vim.api.nvim_set_hl(0, "LspCodeLens", { bg = "NONE" })
+						vim.lsp.codelens.enable()
+					end
+				end
+			end,
+		})
+
+		vim.lsp.config("lua_ls", {
+			settings = {
+				Lua = {
+					diagnostic = {
+						globals = { "vim" },
+					},
+				},
+			},
 		})
 	end,
 }
