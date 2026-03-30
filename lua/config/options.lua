2325f33cd22f8c80682a50cabbbe41d448339c06
Nias26 <genangeliludovico26@gmail.com>
2026-03-30 11:01:56 +0200

    style: format

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,5 @@
 -- stylua: ignore start
-vim.g.showcmdloc = "statusline"                                                     -- Show command message location
+vim.g.showcmdloc = "statusline"                        -- Show command message location
 
 -- Disable language provider support
 vim.g.loaded_perl_provider = 0
@@ -9,39 +9,39 @@ vim.g.loaded_python_provider = 0
 vim.g.loaded_python3_provider = 0
 
 
-vim.opt.sessionoptions = {                                                          -- Required by scope.nvim
+vim.opt.sessionoptions = {                             -- Required by scope.nvim
   "buffers",
   "tabpages",
   "globals",
 }
 
 local options = {
-  relativenumber = false,                                                           -- Set Relative Number line
-  number = false,                                                                   -- Set Number line
-  inccommand = "split",                                                             -- Command preview (eg. `%s/.../...`)
-  ignorecase = true,                                                                -- Ignore uppercase and lowercase in commands
-  infercase = true,                                                                 -- Search for unsensitive case
-  laststatus = 3,                                                                   -- Set global statusbar
-  scrolloff = 4,                                                                    -- Lines above and under cursorline
-  updatetime = 250,                                                                 -- Set updatetime for events
-  timeoutlen = 250,                                                                 -- Timeout for events
-  showmode = false,                                                                 -- Hide --INSERT-- messages etc...
-  splitbelow = true,                                                                -- Always split below
-  splitright = true,                                                                -- Always split right
-  undofile = true,                                                                  -- Enable persistent undo
-  cursorline = true,                                                                -- Enable cursorline
-  termguicolors = true,                                                             -- Enable termguicolors
-  tabstop = 2,                                                                      -- Set tabstop
-  softtabstop = 2,                                                                  -- Soft tabstop
-  shiftwidth = 2,                                                                   -- Set shiftwidth
-  expandtab = true,                                                                 -- Expand tabs into spaces
-  autochdir = true,                                                                 -- Auto change directory
-  clipboard = "unnamedplus",                                                        -- Sync with system clipboard
-  wrap = false,                                                                     -- Disable line wrap
-  grepprg = "rg --vimgrep --smart-case",                                            -- Use ripgrep instead of grep
-  grepformat = "%f:%l:%c:%m",                                                       -- Grep formatting
-  formatexpr = "v:lua.require'conform'.formatexpr()",                               -- Format function
-  mousemodel = "extend",                                                            -- Right click extend selection
+  relativenumber = false,                              -- Set Relative Number line
+  number = false,                                      -- Set Number line
+  inccommand = "split",                                -- Command preview (eg. `%s/.../...`)
+  ignorecase = true,                                   -- Ignore uppercase and lowercase in commands
+  infercase = true,                                    -- Search for unsensitive case
+  laststatus = 3,                                      -- Set global statusbar
+  scrolloff = 4,                                       -- Lines above and under cursorline
+  updatetime = 250,                                    -- Set updatetime for events
+  timeoutlen = 250,                                    -- Timeout for events
+  showmode = false,                                    -- Hide --INSERT-- messages etc...
+  splitbelow = true,                                   -- Always split below
+  splitright = true,                                   -- Always split right
+  undofile = true,                                     -- Enable persistent undo
+  cursorline = true,                                   -- Enable cursorline
+  termguicolors = true,                                -- Enable termguicolors
+  tabstop = 2,                                         -- Set tabstop
+  softtabstop = 2,                                     -- Soft tabstop
+  shiftwidth = 2,                                      -- Set shiftwidth
+  expandtab = true,                                    -- Expand tabs into spaces
+  autochdir = true,                                    -- Auto change directory
+  clipboard = "unnamedplus",                           -- Sync with system clipboard
+  wrap = false,                                        -- Disable line wrap
+  grepprg = "rg --vimgrep --smart-case",               -- Use ripgrep instead of grep
+  grepformat = "%f:%l:%c:%m",                          -- Grep formatting
+  formatexpr = "v:lua.require'conform'.formatexpr()",  -- Format function
+  mousemodel = "extend",                               -- Right click extend selection
   fillchars = {
     eob = " ",
     vert = "│",
@@ -51,18 +51,18 @@ local options = {
     foldclose = "",
     fold = " ",
     msgsep = "─",
-  },                                                                                -- Fill chars
+  },                                                   -- Fill chars
   listchars = {
     trail = "·",
-  },                                                                                -- List chars
-  conceallevel = 2,                                                                 -- Font conceal
-  signcolumn = "yes:1",                                                             -- Draw signs on statuscolumn
-  writebackup = false,                                                              -- No backups of files
-  swapfile = false,                                                                 -- No swap file
-  smartcase = true,                                                                 -- Ignore ignorecase if case all uppercase
-  gdefault = true,                                                                  -- Always substitute globally
-  path = { ".", "**" },                                                             -- Search path
-  splitkeep = "screen",                                                             -- Keep text on screen line
+  },                                                   -- List chars
+  conceallevel = 2,                                    -- Font conceal
+  signcolumn = "yes:1",                                -- Draw signs on statuscolumn
+  writebackup = false,                                 -- No backups of files
+  swapfile = false,                                    -- No swap file
+  smartcase = true,                                    -- Ignore ignorecase if case all uppercase
+  gdefault = true,                                     -- Always substitute globally
+  path = { ".", "**" },                                -- Search path
+  splitkeep = "screen",                                -- Keep text on screen line
 }
 
 for k, v in pairs(options) do
