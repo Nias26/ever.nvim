ac977469a21d56ed81bf46b0e342b0ecdd208afb
Nias26 <genangeliludovico26@gmail.com>
2026-04-03 00:00:30 +0200

    perf: change vim-visual-multi loading time

--- lua/plugins/extra/vim-visual-multi.lua
+++ lua/plugins/extra/vim-visual-multi.lua
@@ -1,11 +1,10 @@
 return {
 	"mg979/vim-visual-multi",
-	event = "BufEnter",
-	init = function()
+	event = { "BufNewFile", "BufReadPost" },
+	config = function()
 		vim.g.VM_set_statusline = 0
 		vim.g.VM_mouse_mappings = 0
 		vim.g.VM_maps = {
-			-- Experimental
 			["Undo"] = "u",
 			["Redo"] = "<C-r>",
 			["Exit"] = "<Esc>",
