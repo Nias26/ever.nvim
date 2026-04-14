f2664fdcad856c0d4270211a0fc5a323c1df59d2
Nias26 <genangeliludovico26@gmail.com>
2026-04-14 19:01:35 +0200

    feat(oil): list oil buffer and keep it open

--- lua/plugins/editor/oil.lua
+++ lua/plugins/editor/oil.lua
@@ -20,7 +20,13 @@ return {
 		require("oil").setup({
 			default_file_explorer = true,
 			columns = { "permissions", "size", "mtime", "icon" },
+			buf_options = {
+				buflisted = true,
+				bufhidden = "hide",
+			},
 			skip_confirm_for_simple_edits = true,
+			prompt_save_on_select_new_entry = false,
+			cleanup_delay_ms = false,
 			constrain_cursor = "editable",
 			watch_for_changes = true,
 			view_options = {
