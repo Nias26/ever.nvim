8dfc06cc6412ac7b9330f39c1557e3a3433dc19c
Nias26 <genangeliludovico26@gmail.com>
2026-04-14 23:24:44 +0200

    doc: just a todo

--- init.lua
+++ init.lua
@@ -3,3 +3,5 @@ require("config.options")
 require("config.keymaps")
 require("config.tabline").setup()
 require("config.autocmds")
+
+-- TODO: Add gitsigns.nvim and show lsp virtual lines only when cursor is on the same line
