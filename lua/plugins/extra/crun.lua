57dd444d0b265031b9c85cd70b36ca284092f721
Nias26 <genangeliludovico26@gmail.com>
2026-04-17 20:40:49 +0200

    feat: update plugin setup

--- lua/plugins/extra/crun.lua
+++ lua/plugins/extra/crun.lua
@@ -1,4 +1,10 @@
 return {
 	"Nias26/crun.nvim",
-	cmd = "Crun",
+	cmd = { "Cc", "Ckill" },
+	keys = {
+		{ "K", "<cmd>Ckill<CR>", desc = "Kill current crun process", ft = "qf" },
+	},
+	opts = {
+		completiom = "path",
+	},
 }
