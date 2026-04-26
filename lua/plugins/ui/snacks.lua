f76ec53355adab4807bfbb53c41d110fc7f73f18
Nias26 <genangeliludovico26@gmail.com>
2026-04-26 19:49:38 +0200

    fix(snacks): initializa snacks properly

--- lua/plugins/ui/snacks.lua
+++ lua/plugins/ui/snacks.lua
@@ -4,11 +4,21 @@ return {
 	lazy = false,
 	opts = {
 		bigfile = { enabled = true },
-		indent = { enabled = true },
+		indent = {
+			enabled = true,
+			filter = function(buf, _)
+				return vim.bo[buf].filetype ~= "dashboard"
+					and vim.bo[buf].filetype ~= "help"
+					and vim.bo[buf].filetype ~= "telescope"
+			end,
+		},
 		quickfile = { enabled = true },
 	},
-	config = function()
+	config = function(_, opts)
 		vim.api.nvim_set_hl(0, "SnacksNormal", { bg = "NONE" })
+
+		-- This is stupid anyways
+		require("snacks").setup(opts)
 
 		vim.api.nvim_create_autocmd("User", {
 			pattern = "VeryLazy",
