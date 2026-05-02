5def424ed03b0b99d7e6e690f273bd42c7a1a985
Nias26 <genangeliludovico26@gmail.com>
2026-05-02 22:02:21 +0200

    feat(keys): change lsp diagnostic jump to be coesive with others '[' ']' keymaps

--- lua/plugins/lsp/lspsaga.lua
+++ lua/plugins/lsp/lspsaga.lua
@@ -7,11 +7,11 @@ return {
 		{ "nvim-tree/nvim-web-devicons", lazy = true },
 	},
 	keys = {
-    { "cz", "<cmd>Lspsaga code_action<CR>", desc = "Code Actions" },
-    { "cd", "<cmd>Lspsaga hover_doc<CR>", desc = "Code Documentation" },
-    { "gr", "<cmd>Lspsaga finder<CR>", desc = "Find References" },
-    { "c[", "<cmd>Lspsaga diagnostic_jump_prev<CR>", desc = "Previous Diagnostic" },
-    { "c]", "<cmd>Lspsaga diagnostic_jump_next<CR>", desc = "Next Diagnostic" },
+		{ "cz", "<cmd>Lspsaga code_action<CR>", desc = "Code Actions" },
+		{ "cd", "<cmd>Lspsaga hover_doc<CR>", desc = "Code Documentation" },
+		{ "gr", "<cmd>Lspsaga finder<CR>", desc = "Find References" },
+		{ "[c", "<cmd>Lspsaga diagnostic_jump_prev<CR>", desc = "Previous Diagnostic" },
+		{ "]c", "<cmd>Lspsaga diagnostic_jump_next<CR>", desc = "Next Diagnostic" },
 	},
 	opts = {
 		diagnostic = {
