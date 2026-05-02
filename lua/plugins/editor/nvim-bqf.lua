0a91a227d152c4fbe992b0b5d21dfa2bf882ebd9
Nias26 <genangeliludovico26@gmail.com>
2026-05-02 22:40:23 +0200

    feat(nvim-bqf): use fzf

--- lua/plugins/editor/nvim-bqf.lua
+++ lua/plugins/editor/nvim-bqf.lua
@@ -1,10 +1,24 @@
 return {
 	"kevinhwang91/nvim-bqf",
 	ft = "qf",
+	dependencies = {
+		{
+			"junegunn/fzf",
+			lazy = true,
+			build = function()
+				vim.fn["fzf#install"]()
+			end,
+		},
+	},
 	opts = {
 		preview = {
 			border = "single",
 			winblend = 0,
+		},
+		filter = {
+			fzf = {
+				extra_opts = { "--bind", "ctrl-o:toggle-all", "--delimiter", "│" },
+			},
 		},
 	},
 }
