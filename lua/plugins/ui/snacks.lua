b262144aa693903338a3dc42e5b935493d0286c8
Nias26 <genangeliludovico26@gmail.com>
2026-05-04 22:38:25 +0200

    feat(snacks): disable indent on whick-key and markdown

--- lua/plugins/ui/snacks.lua
+++ lua/plugins/ui/snacks.lua
@@ -6,29 +6,26 @@ return {
 		bigfile = { enabled = true },
 		indent = {
 			enabled = true,
-			filter = function(buf, _)
-				return vim.bo[buf].filetype ~= "dashboard"
-					and vim.bo[buf].filetype ~= "help"
+			filter = function(buf)
+				return vim.g.snacks_indent ~= false
+					and vim.b[buf].snacks_indent ~= false
+					and vim.bo[buf].buftype == ""
+					and vim.bo[buf].filetype ~= "wk"
 					and vim.bo[buf].filetype ~= "TelescopePrompt"
+					and vim.bo[buf].filetype ~= "dashboard"
+					and vim.bo[buf].filetype ~= "help"
 					and vim.bo[buf].filetype ~= "noice"
 					and vim.bo[buf].filetype ~= "lazy"
-					and vim.bo[buf].filetype ~= "wk"
-					and vim.bo[buf].buftype ~= "prompt"
+					and vim.bo[buf].filetype ~= "markdown"
 			end,
 		},
 		quickfile = { enabled = true },
+		-- TODO: Use the picker
 	},
 	config = function(_, opts)
 		vim.api.nvim_set_hl(0, "SnacksNormal", { bg = "NONE" })
 
 		-- This is stupid anyways
 		require("snacks").setup(opts)
-
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "VeryLazy",
-			callback = function()
-				Snacks.scroll.enable()
-			end,
-		})
 	end,
 }
