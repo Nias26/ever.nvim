c39026b8795af9cbdebe8a2c90e2084eadeebecd
Nias26 <genangeliludovico26@gmail.com>
2026-05-06 09:55:46 +0200

    doc: todo

--- init.lua
+++ init.lua
@@ -6,3 +6,4 @@ require("config.quickfix")
 require("config.autocmds")
 
 -- TODO: Add gitsigns.nvim
+-- TODO: Fix treesitter not enabling automatically (vim.treesitter.start() must be called manually some times)
