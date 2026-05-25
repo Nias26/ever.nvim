b546576a46f35568179316d8903f85a36bf277c6
Nias26 <genangeliludovico26@gmail.com>
2026-05-25 11:00:10 +0200

    fix(treesitter): no autostart

--- lua/plugins/editor/nvim-treesitter.lua
+++ lua/plugins/editor/nvim-treesitter.lua
@@ -1,14 +1,31 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPost", "BufNewFile" },
+	lazy = false,
 	dependencies = {
 		{ "folke/ts-comments.nvim", opts = {} },
 	},
 	build = ":TSUpdate",
 	config = function()
-    require("nvim-treesitter").install({ "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "regex" })
+		local ts = require("nvim-treesitter")
+		ts.install({
+			"c",
+			"lua",
+			"vim",
+			"vimdoc",
+			"query",
+			"markdown",
+			"markdown_inline",
+			"regex",
+		})
+
 		vim.filetype.add({
 			pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(opts)
+				pcall(vim.treesitter.start, opts.buf)
+			end,
 		})
 	end,
 }
