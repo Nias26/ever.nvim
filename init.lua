c0544770f438094f74311a7a87144c6a03dddb46
Nias26 <genangeliludovico26@gmail.com>
2026-05-25 11:00:47 +0200

    doc: remove todo

--- init.lua
+++ init.lua
@@ -6,4 +6,6 @@ require("config.quickfix")
 require("config.autocmds")
 
 -- TODO: Add gitsigns.nvim
--- TODO: Fix treesitter not enabling automatically (vim.treesitter.start() must be called manually some times)
+-- TODO: Create Terminal command for vsplit term, split term, floating term, new tab term or whole buffer term
+-- TODO: fix ts_ls sending double requests (like open documentation)
+-- TODO: Fix Conform failing on js files
