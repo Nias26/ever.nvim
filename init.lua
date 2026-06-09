3881d32fee1e22927530dfe3dfb2a8a4c7984f08
Nias26 <genangeliludovico26@gmail.com>
2026-06-09 18:15:47 +0200

    doc: add new todo

--- init.lua
+++ init.lua
@@ -8,3 +8,5 @@ require("config.autocmds")
 -- TODO: Add gitsigns.nvim
 -- TODO: Create Terminal command for vsplit term, split term, floating term, new tab term or whole buffer term
 -- TODO: Fix Conform failing on js files
+-- TODO: Fix 'K' keymap getting pressed multiple times
+-- TODO: Fix 'Q' closing all buffers when inside a tab. Instead switch to other buffer and if none, open a new buffer. Never close tab until said with ':tabclose'
