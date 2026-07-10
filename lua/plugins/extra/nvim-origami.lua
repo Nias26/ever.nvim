bc8c541ab426717dd86bf36523b43c9048e74e89
Nias26 <genangeliludovico26@gmail.com>
2026-07-10 22:43:42 +0200

    feat(plugins): replace nvim-ufo with nvim-origami

---
+++ lua/plugins/extra/nvim-origami.lua
@@ -0,0 +1,13 @@
+return {
+	"chrisgrieser/nvim-origami",
+	event = "VeryLazy",
+	opts = {
+		foldText = {
+			disableOnFt = { "Trouble", "lazy", "dashboard", "TelescopePrompt", "neo-tree", "" },
+		},
+	},
+	config = function(_, opts)
+		require("origami").setup(opts)
+		vim.api.nvim_set_hl(0, "FoldColumn", { fg = "#525252", bg = "NONE" })
+	end,
+}
