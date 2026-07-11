ceda690b42220ed6e4f3a1ea70750183f4ab46e0
Nias26 <genangeliludovico26@gmail.com>
2026-07-11 21:40:45 +0200

    feat(plugins): wrap crun keybinds to compile-mode.nvim

--- lua/plugins/extra/compile-mode.lua
+++ lua/plugins/extra/compile-mode.lua
@@ -1,6 +1,10 @@
 return {
 	"ej-shafran/compile-mode.nvim",
 	branch = "latest",
+	cmd = { "Compile", "Recompile", "Cc" },
+	keys = {
+		{ mode = "n", "!", ":Cc " },
+	},
 	dependencies = {
 		{ "nvim-lua/plenary.nvim", lazy = true },
 	},
@@ -8,5 +12,13 @@ return {
 		vim.g.compile_mode = {
 			input_word_completion = true,
 		}
+
+		vim.api.nvim_create_user_command("Cc", function(args)
+			if #args.args < 1 then
+				vim.cmd("Recompile")
+			else
+				vim.cmd("Compile " .. args.args)
+			end
+		end, { nargs = "*" })
 	end,
 }
