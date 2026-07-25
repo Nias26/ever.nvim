8c841a00db3fb48833fe9b2480838a214c461676
Nias26 <genangeliludovico26@gmail.com>
2026-07-25 22:41:04 +0200

    feat(format): do not format after every save. Format manually.
    Maybe this will make me write better aligned code

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -1,36 +1,40 @@
 return {
-	"stevearc/conform.nvim",
-	event = "BufWritePre",
-	cmd = "ConformInfo",
-	opts = {
-		log_level = vim.log.levels.DEBUG,
-		formatters = {
-			clang_format = {
-				append_args = {
-					"--style={BasedOnStyle:LLVM",
-					"IndentCaseLabels:true}",
-				},
-			},
-		},
-		formatters_by_ft = {
-			lua = { "stylua" },
-			cpp = { "clang-format" },
-			c = { "clang-format" },
-			python = { "black" },
-			rust = { "rustfmt" },
-			typescript = { "prettierd" },
-			javascript = { "prettierd" },
-			svelte = { "prettierd" },
-			css = { "prettierd" },
-			scss = { "prettierd" },
-			html = { "prettierd" },
-			markdown = { "prettierd" },
-			yaml = { "prettierd" },
-			["_"] = { "trim_whitespace" },
-		},
-		format_after_save = {
-			timeout_ms = 500,
-			lsp_format = "fallback",
-		},
-	},
+  "stevearc/conform.nvim",
+  cmd = "ConformInfo",
+  keys = { {
+    "cf",
+    function()
+      require("conform").format({
+        async = true,
+      })
+    end,
+    desc = "Format code",
+  } },
+  init = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
+  opts = {
+    log_level = vim.log.levels.DEBUG,
+    formatters = { clang_format = { append_args = { "--style={BasedOnStyle:LLVM}" } } },
+    formatters_by_ft = {
+      lua = {
+        "stylua",
+      },
+      cpp = { "clang-format" },
+      c = { "clang-format" },
+      python = {
+        "black",
+      },
+      rust = { "rustfmt" },
+      typescript = { "prettierd" },
+      javascript = { "prettierd" },
+      svelte = { "prettierd" },
+      css = { "prettierd" },
+      scss = { "prettierd" },
+      html = { "prettierd" },
+      markdown = { "prettierd" },
+      yaml = { "prettierd" },
+      ["_"] = { "trim_whitespace" },
+    },
+  },
 }
