73192f48b74cb7476299b0181935080eca424dac
Nias26 <genangeliludovico26@gmail.com>
2026-07-27 14:42:11 +0200

    feat(conform): enable range formatting

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -1,40 +1,43 @@
 return {
-  "stevearc/conform.nvim",
-  cmd = "ConformInfo",
-  keys = { {
-    "cf",
-    function()
-      require("conform").format({
-        async = true,
-      })
-    end,
-    desc = "Format code",
-  } },
-  init = function()
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
-  opts = {
-    log_level = vim.log.levels.DEBUG,
-    formatters = { clang_format = { append_args = { "--style={BasedOnStyle:LLVM}" } } },
-    formatters_by_ft = {
-      lua = {
-        "stylua",
-      },
-      cpp = { "clang-format" },
-      c = { "clang-format" },
-      python = {
-        "black",
-      },
-      rust = { "rustfmt" },
-      typescript = { "prettierd" },
-      javascript = { "prettierd" },
-      svelte = { "prettierd" },
-      css = { "prettierd" },
-      scss = { "prettierd" },
-      html = { "prettierd" },
-      markdown = { "prettierd" },
-      yaml = { "prettierd" },
-      ["_"] = { "trim_whitespace" },
-    },
-  },
+	"stevearc/conform.nvim",
+	cmd = "ConformInfo",
+	keys = {
+		{
+			mode = { "n", "v" },
+			"cf",
+			function()
+				require("conform").format({
+					async = true,
+				})
+			end,
+			desc = "Format code",
+		},
+	},
+	init = function()
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
+	opts = {
+		log_level = vim.log.levels.DEBUG,
+		formatters = { clang_format = { append_args = { "--style={BasedOnStyle:LLVM}" } } },
+		formatters_by_ft = {
+			lua = {
+				"stylua",
+			},
+			cpp = { "clang-format" },
+			c = { "clang-format" },
+			python = {
+				"black",
+			},
+			rust = { "rustfmt" },
+			typescript = { "prettierd" },
+			javascript = { "prettierd" },
+			svelte = { "prettierd" },
+			css = { "prettierd" },
+			scss = { "prettierd" },
+			html = { "prettierd" },
+			markdown = { "prettierd" },
+			yaml = { "prettierd" },
+			["_"] = { "trim_whitespace" },
+		},
+	},
 }
