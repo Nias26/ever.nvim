3af19e4d5aee79aed6e33932048d1213c64d0c4b
Nias26 <genangeliludovico26@gmail.com>
2026-08-06 23:30:43 +0200

    doc: remove comment

--- lua/plugins/ui/colorschemes.lua
+++ lua/plugins/ui/colorschemes.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"nyoom-engineering/oxocarbon.nvim",
 		config = function()
-			vim.opt.background = "dark" -- set this to dark or light
+			vim.opt.background = "dark"
 		end,
 	},
 	{
