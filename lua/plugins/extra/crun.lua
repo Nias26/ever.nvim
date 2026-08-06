67babc1b4219a370403abbf3093ffa35a1921c77
Nias26 <genangeliludovico26@gmail.com>
2026-08-06 23:29:55 +0200

    feat(plugins): bring back Crun

--- lua/plugins/extra/crun.lua
+++ lua/plugins/extra/crun.lua
@@ -1,27 +1,11 @@
 return {
 	"Nias26/crun.nvim",
-	enabled = false,
-	cmd = { "Cc", "Cnext", "Cprev", "Cqf", "Ckill", "Ctoggle" },
+	cmd = { "Cc", "Ckill", "Ctoggle" },
 	keys = {
 		{ "K", "<cmd>Ckill<CR>", desc = "Kill current crun process", ft = "crun" },
 		{ "!", ":Cc ", desc = "Exec command on Quickfix" },
 		{ "<A-c>", "<cmd>Ctoggle<CR>", desc = "Toggle Crun buffer" },
 	},
 	opts = {
-		completion = "path",
-		timestamps = true,
-		color = true,
-		errorformat = nil,
-		quickfix = {
-			enabled = true,
-			open = false,
-		},
-		window = {
-			position = "bottom",
-			height = 15,
-			width = 80,
-			name = "[Crun]",
-		},
-		echo = false,
 	},
 }
