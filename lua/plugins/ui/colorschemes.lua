178699b8629d79f1a4a4072f7a91c2a1808f01ec
Nias26 <genangeliludovico26@gmail.com>
2026-08-18 11:32:05 +0200

    style(colorscheme): remove vague and add kanagawa

--- lua/plugins/ui/colorschemes.lua
+++ lua/plugins/ui/colorschemes.lua
@@ -14,9 +14,23 @@ return {
 		end,
 	},
 	{
-		"vague-theme/vague.nvim",
-		lazy = false,
-		priority = 1000,
+		"rebelot/kanagawa.nvim",
+		opts = {
+			commentStyle = { italic = false },
+			keywordStyle = { italic = false },
+			overrides = function(colors)
+				local theme = colors.theme
+				return {
+					TelescopeTitle = { fg = theme.ui.special, bold = true },
+					TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+					TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+					TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+					TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+					TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+					TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+				}
+			end,
+		},
 	},
 	{
 		"xiyaowong/transparent.nvim",
