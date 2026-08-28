a4f3e37cb43eeced59737c7bb0b294908a6bb398
Nias26 <genangeliludovico26@gmail.com>
2026-08-28 09:41:52 +0200

    doc: todo

--- init.lua
+++ init.lua
@@ -3,7 +3,3 @@ require("config.options")
 require("config.keymaps")
 require("config.tabline").setup()
 require("config.autocmds")
-
--- TODO: Fix Conform failing on js files
--- TODO: Fix 'K' keymap getting pressed multiple times
--- TODO: Set some formatting preferences on clang-format inside conform.nvim
