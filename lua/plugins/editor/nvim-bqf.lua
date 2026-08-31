6735798ae4f2d84362c97cc07ec027857bda7b97
Nias26 <genangeliludovico26@gmail.com>
2026-08-31 21:33:03 +0200

    feat(quickfix): disable auto-preview

--- lua/plugins/editor/nvim-bqf.lua
+++ lua/plugins/editor/nvim-bqf.lua
@@ -11,11 +11,10 @@ return {
 		preview = {
 			border = "single",
 			winblend = 0,
+			auto_preview = false, 
 		},
-		filter = {
-			fzf = {
-				extra_opts = { "--bind", "ctrl-o:toggle-all", "--delimiter", "│" },
-			},
+		fzf = {
+			extra_opts = { "--bind", "ctrl-o:toggle-all", "--delimiter", "│" },
 		},
 	},
 }
